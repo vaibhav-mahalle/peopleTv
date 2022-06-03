a3d635e90c28a274d523b85f3180b3937994959e
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-06-03 16:22:16 +0530

    added categories, videos in the backend

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,26 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Computer Programming",
+    categoryName: "Norm",
     description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+      "Norm Macdonald is a comedic genius who was considered as funniest man by fellow comics of his time",
   },
   {
     _id: uuid(),
-    categoryName: "Frontend Development",
+    categoryName: "Ricky",
     description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
+      "Ricky gervais is the British comic, actor, creator known for The Office and roasting celebrities on biggest award shows.",
   },
   {
     _id: uuid(),
-    categoryName: "Backend Development",
+    categoryName: "Sumit",
     description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
+      "Sumit Anand is an Indian comic who has only couple of videos on youtube but he is as funny as they come.",
+  },
+  {
+    _id: uuid(),
+    categoryName: "Upmanyu",
+    description:
+      "Abhishek Upmanyu is the Indian standup comedian famous for his unique delivery style and excellent standup acts.",
   },
 ];
