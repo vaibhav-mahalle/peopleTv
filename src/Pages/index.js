10807045ed26a618cf5b363d2bd2dcee0904ea10
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2022-06-14 09:07:08 +0530

    chore: minor import updates and minor changes

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,7 +3,7 @@ export {Explore} from './Explore';
 export {History} from './History';
 export {Home} from './Home';
 export {Liked} from './Liked';
-export {PlayList} from './Playlist';
+export {PlayList, SinglePlaylist} from './Playlist';
 export {WatchLater} from './WatchLater';
 export {VideoPage} from './VideoPage';
 export {Login, Signup} from './Auth';
